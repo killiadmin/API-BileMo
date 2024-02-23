9f92d116b6f5cb37624b5a27a12fa9df33a46664
killiadmin <k.filatre@gmail.com>
2024-02-23 15:57:31 +0100

    create buyer entity

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,12 +4,21 @@ namespace App\DataFixtures;
 
 use App\Entity\User;
 use App\Entity\Product;
+use App\Entity\Buyer;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
         // Instantiating the Faker generator
@@ -25,12 +34,23 @@ class AppFixtures extends Fixture
 
         // Creation of 20 clients with 10 users associated
         for ($i=0; $i < 20; $i++) {
-            $client = new User();
-            $client->setCompany($faker->company);
-            $client->setEmail($faker->unique()->safeEmail);
-            $client->setPassword($faker->password());
-            $client->setRoles(['ROLES_ADMIN']);
-            $manager->persist($client);
+            $user = new User();
+            $user->setCompany($faker->company);
+            $user->setEmail($faker->unique()->safeEmail);
+            $user->setPassword($this->passwordHasher->hashPassword($user, "password"));
+            $user->setRoles(['ROLES_ADMIN']);
+            $manager->persist($user);
+
+            for ($j=0; $j < 10; $j++) {
+                $buyer = new Buyer();
+                $buyer->setFirstname($faker->firstName);
+                $buyer->setLastname($faker->lastName);
+                $buyer->setEmail($faker->unique()->safeEmail);
+                $buyer->setAddress($faker->address());
+                $buyer->setPhone($faker->phoneNumber());
+                $buyer->setCompanyAssociated($user);
+                $manager->persist($buyer);
+            }
         }
 
         $manager->flush();
