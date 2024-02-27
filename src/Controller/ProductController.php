e104130c3477891a3934e9138cdaade47845e2f0
Killian FILÂTRE <k.filatre@cicd.biz>
2024-02-27 11:24:57 +0100

    add method for pagination products datas

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,31 +4,48 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Repository\ProductRepository;
+use Psr\Cache\InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 class ProductController extends AbstractController
 {
     /**
-     * Retrieves all products from the database.
+     * Retrieves all products with pagination.
      *
      * @param ProductRepository $productRepository The product repository.
-     * @param SerializerInterface $serializer The serializer used to convert the products list to JSON.
+     * @param SerializerInterface $serializer The serializer interface.
+     * @param Request $request The request object.
+     * @param TagAwareCacheInterface $cachePool The cache pool.
      *
-     * @return JsonResponse The response containing the list of products in JSON format.
+     * @return JsonResponse The JSON response containing all products.
+     * @throws InvalidArgumentException
      */
     #[Route('/api/products', name: 'app_products')]
     public function getAllProducts
     (
-        ProductRepository   $productRepository,
-        SerializerInterface $serializer
+        ProductRepository      $productRepository,
+        SerializerInterface    $serializer,
+        Request                $request,
+        TagAwareCacheInterface $cachePool
     ): JsonResponse
     {
-        $productsList = $productRepository->findAll();
-        $jsonProductsList = $serializer->serialize($productsList, 'json');
+        $page = $request->get('page', 1);
+        $limit = $request->get('limit', 3);
+
+        $idCache = "getAllProducts-" . $page . "-" . $limit;
+
+        $jsonProductsList = $cachePool->get($idCache, function (ItemInterface $item) use ($productRepository, $page, $limit, $serializer) {
+            $item->tag('productsCache');
+            $productsList = $productRepository->findAllWithPagination($page, $limit);
+            return $serializer->serialize($productsList, 'json');
+        });
 
         return new JsonResponse($jsonProductsList, Response::HTTP_OK, [], true);
     }
