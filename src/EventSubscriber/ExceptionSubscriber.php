b022a73a0d07ba888ba1a146736b71ed0f629482
Killian FILÂTRE <k.filatre@cicd.biz>
2024-03-20 16:43:43 +0100

    redirect route for the uses documentation

--- src/EventSubscriber/ExceptionSubscriber.php
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -4,14 +4,16 @@ namespace App\EventSubscriber;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
 class ExceptionSubscriber implements EventSubscriberInterface
 {
     /**
-     * Handles kernel exceptions and sets an appropriate response.
+     * Handles kernel exceptions and sets the appropriate response based on the exception type.
      *
      * @param ExceptionEvent $event The exception event.
      * @return void
@@ -20,20 +22,22 @@ class ExceptionSubscriber implements EventSubscriberInterface
     {
         $exception = $event->getThrowable();
 
-        if ($exception instanceof HttpException) {
+        if ($exception instanceof HttpException && $exception->getStatusCode() === 404) {
+            $event->setResponse(new RedirectResponse('/api/doc'));
+        } elseif ($exception instanceof HttpException) {
             $data = [
                 'status' => $exception->getStatusCode(),
                 'message' => $exception->getMessage()
             ];
+            $event->setResponse(new JsonResponse($data));
 
         } else {
             $data = [
-                'status' => 500, // The status does not exist because it is not an HTTP exception, so we set 500 by default.
+                'status' => 500,
                 'message' => $exception->getMessage()
             ];
-
+            $event->setResponse(new JsonResponse($data));
         }
-        $event->setResponse(new JsonResponse($data));
     }
 
     /**
